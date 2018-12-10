c1ff3b773defbc5459621d75ee5130798a5f6e6c
Cara Hill <cara@rabidtech.co.nz>
2018-12-10 16:10:52 +1300

    All tests passing.

--- food-chain.js
+++ food-chain.js
@@ -1,52 +1,75 @@
 class Song {
   constructor() {
-    this.verses = { 1: "fly",
-                    2: "spider",
-                    3: "bird",
-                    4: "cat",
-                    5: "dog",
-                    6: "goat",
-                    7: "cow",
-                    8: "horse" }
+    this.critters = { 1: "fly",
+                      2: "spider",
+                      3: "bird",
+                      4: "cat",
+                      5: "dog",
+                      6: "goat",
+                      7: "cow",
+                      8: "horse" }
 
-    this.secondaryVerses = { "spider": `It wriggled and jiggled and tickled inside her.`,
-                             "bird": `How absurd to swallow a bird!`,
-                             "cat": `Imagine that, to swallow a cat!`,
-                             "dog": `What a hog, to swallow a dog!`,
-                             "goat": `Just opened her throat and swallowed a goat!`,
-                             "cow": `I don't know how she swallowed a cow!`,
-                             "horse": `She's dead, of course!` }
+    this.critterVerses = { 1: `I know an old lady who swallowed a fly.`,
+                    2: `I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.`,
+                    3: `I know an old lady who swallowed a bird.
+How absurd to swallow a bird!`,
+                    4: `I know an old lady who swallowed a cat.
+Imagine that, to swallow a cat!`,
+                    5: `I know an old lady who swallowed a dog.
+What a hog, to swallow a dog!`,
+                    6: `I know an old lady who swallowed a goat.
+Just opened her throat and swallowed a goat!`,
+                    7: `I know an old lady who swallowed a cow.
+I don't know how she swallowed a cow!`,
+                    8: `I know an old lady who swallowed a horse.
+She's dead, of course!
+`   }
+  }
+
+  verses(number, secondNumber) {
+    let song = "";
+    let count = number;
+
+    for (count; count <= secondNumber; count++) {
+      song += this.verse(count);
+      song += `\n`;
+    }
+
+    return song;
   }
 
   verse(number) {
-    let creature = this.verses[number];
-    if (number === 8) {
-      return `I know an old lady who swallowed a horse.
-She's dead, of course!
-`;
-    } else if (number > 1) {
-      return `I know an old lady who swallowed a ${creature}.
-${this.secondaryVerses[creature]}
+    if (number === 8) return this.critterVerses[number];
+
+    let song = "";
+
+    song += this.critterVerses[number];
+
+    if (number > 1) {
+      song += `
 ${this.swallows(number)}
 I don't know why she swallowed the fly. Perhaps she'll die.
-`; } else {
-  return `I know an old lady who swallowed a ${creature}.
+`;  } else {
+      song += `
 I don't know why she swallowed the fly. Perhaps she'll die.
-`; }
+`;  }
+
+    return song;
   }
 
-  swallows(number) {
+  swallows(finalVerse) {
     let swallowVerses = "";
-    let verseCount = number;
-    for (let i = 0; i <= number; i++) {
-      if (this.verses[verseCount] === "bird") {
+    let verseCount = finalVerse;
+
+    for (let i = 0; i <= finalVerse; i++) {
+      if (this.critters[verseCount] === "bird") {
         swallowVerses += this.spider();
         verseCount -= 1;
-      } else if (this.verses[verseCount] === "fly") {
+      } else if (this.critters[verseCount] === "fly") {
         swallowVerses += "";
       } else {
-        swallowVerses += `She swallowed the ${this.verses[verseCount]} to catch the ${this.verses[verseCount -= 1]}.`;
-
+        swallowVerses += `She swallowed the ${this.critters[verseCount]} to catch the ${this.critters[verseCount -= 1]}.`;
       }
 
       if (verseCount >= 2) swallowVerses += `\n`;
--- food-chain.spec.js
+++ food-chain.spec.js
@@ -98,7 +98,7 @@ She's dead, of course!
     expect(song.verse(8)).toEqual(expected);
   });
 
-  xtest('multiple verses', () => {
+  test('multiple verses', () => {
     const expected = `I know an old lady who swallowed a fly.
 I don't know why she swallowed the fly. Perhaps she'll die.
 
@@ -112,7 +112,7 @@ I don't know why she swallowed the fly. Perhaps she'll die.
     expect(song.verses(1, 2)).toEqual(expected);
   });
 
-  xtest('the whole song', () => {
+  test('the whole song', () => {
     const expected = `I know an old lady who swallowed a fly.
 I don't know why she swallowed the fly. Perhaps she'll die.
 
